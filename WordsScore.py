4d9504e56c085ee642db6217208e084179c51639
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2024-01-20 22:01:33 +0530

    Add files via upload

---
+++ WordsScore.py
@@ -0,0 +1,18 @@
+"""
+Title     : Words Score
+Subdomain : Debugging
+Domain    : Python
+Problem   : https://www.hackerrank.com/challenges/words-score/problem
+"""
+
+
+def is_vowel(letter):
+    return letter in ["a", "e", "i", "o", "u", "y"]
+
+
+def score_words(words):
+    score = 0
+    for word in words:
+        num_vowels = sum(1 for letter in word if is_vowel(letter))
+        score += 2 if num_vowels % 2 == 0 else 1
+    return score
